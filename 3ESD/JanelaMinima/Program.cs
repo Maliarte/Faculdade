f530020e7f46e03adbe88ab16becbcb12dfbb462
gilrsantana <g.r.santana@hotmail.com>
2021-09-21 22:38:36 -0300

    Exercicio janela mínima

---
+++ 3ESD/JanelaMinima/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+/*
+ * Construa um algoritmo (em qualquer linguagem) que resolva o problema da Janela Mínima
+ * de uma Substring:
+ * Pegue duas strings s e t, e retorne a janela mínima de uma substring em s, onde cada 
+ * caracter de t (pode incluir duplicatas) está inclusa na janela. 
+ * Se não encontrar a substring, retorne uma string vazia "".
+
+ * Exemplo:
+ * s = "ADOBECODEBANC", t = "ABC" 
+ * "BANC" 
+ * A janela mínima é a substring "BANC", que inclui os caracteres 'A', 'B' e 'C' da string t.
+ */
+
+
+namespace JanelaMinima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "ADOBECODEMYNL";
+            string referencia = "ABL";
+            string janelaMinima;
+            int i = 0;
+            int j = 0;
+            int tamTrechoDoTexto;
+            bool inicia = false;
+
+            char[] arrayT = new char[referencia.Length];
+            arrayT = referencia.ToCharArray();
+            tamTrechoDoTexto = arrayT.Length;
+
+            while (i < referencia.Length)
+            {
+                if (j <= (texto.Length - tamTrechoDoTexto))
+                {
+                    janelaMinima = texto.Substring(j, tamTrechoDoTexto);
+                    if (janelaMinima.Contains(arrayT[i]))
+                    {
+                        inicia = false;
+                        if (i == referencia.Length - 1)
+                        {
+                            System.Console.WriteLine($"Janela mínima: {janelaMinima}");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (tamTrechoDoTexto == texto.Length)
+                        {
+                            System.Console.WriteLine("Correspondência não encontrada.");
+                            break;
+                        }
+                        else
+                        {
+                            if (j >= texto.Length - tamTrechoDoTexto)
+                            {
+                                j = 0;
+                                tamTrechoDoTexto++;
+                            }
+                            else
+                            {
+                                j++;
+                            }
+                            inicia = true;
+                            i = 0;
+                        }
+                    }
+                }
+                if (!inicia)
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
