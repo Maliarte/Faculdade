5b99a34b08071d46dbb43adfaa22261bd77f6984
gilrsantana <g.r.santana@hotmail.com>
2021-10-08 05:20:38 -0300

    Arquivo de configuração com o banco de dados

---
+++ 3DAW/ex04/config.php
@@ -0,0 +1,11 @@
+<?php
+$dbl = "mysql:dbname=3DAW;host=127.0.0.1";
+$dbuser = "gilmar";
+$dbpass = "senha.123";
+
+try {
+    $PDO = new PDO($dbl, $dbuser, $dbpass);
+} catch (PDOException $e) {
+    echo "Falha na conexão<br>".$e->getMessage();
+}
+?>
